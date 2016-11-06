ab268b8aa878376ee660d6285642e44782c1f4dd
Alessio Degani <alessio.degani@gmail.com>
2016-11-06 18:26:04 +0100

    Added note list in order to manage more than one NOTE ON/OFF messages at a time

--- DS-2_MIDI_NANO/DS-2_MIDI_NANO.ino
+++ DS-2_MIDI_NANO/DS-2_MIDI_NANO.ino
@@ -1,11 +1,12 @@
-// Configure the ATMega328 pin names
-#define DS2_GATE_NEG 2
-#define DS2_32o 3
-#define DS2_16o 4
-#define DS2_8o 5
-#define DS2_4o 6
-#define DS2_2o 7
-#define DS2_1o 8
+/* DS-2 MIDI IN NANO
+ *  Add MIDI input capabilities to CRUMAR DS-2
+ *  Author: Alessio Degani, 2016 <alessio.degani@gmail.com>
+ */
+
+#include "pinConfig.h"
+#include "noteList.h"
+
+noteList_t midiNotes;
 
 byte incomingByte;
 byte note;
@@ -17,18 +18,18 @@ void playNote(byte note, byte velocity){
 
   // A note off -> close the gate
   if (velocity == 0) {
-    digitalWrite(DS2_GATE, HIGH);
+    digitalWrite(DS2_GATE_NEG, HIGH);
     return;
   }
   
   if (( note >= 41 ) && ( note <= 84 )) {
     note = note - 40;
-    ((note & 32)==32)? digitalWrite(DS2_32o, HIGH): digitalWrite(DS2_32o, LOW);
-    ((note & 16)==16)? digitalWrite(DS2_16o, HIGH): digitalWrite(DS2_16o, LOW);
-    ((note & 8)==8)? digitalWrite(DS2_8o, HIGH): digitalWrite(DS2_8o, LOW);
-    ((note & 4)==4)? digitalWrite(DS2_4o, HIGH): digitalWrite(DS2_4o, LOW);
-    ((note & 2)==2)? digitalWrite(DS2_2o, HIGH): digitalWrite(DS2_2o, LOW);
-    ((note & 1)==1)? digitalWrite(DS2_1o, HIGH): digitalWrite(DS2_1o, LOW);
+    ( (note&32) == 32 ) ? digitalWrite(DS2_32o, HIGH): digitalWrite(DS2_32o, LOW);
+    ( (note&16) == 16 ) ? digitalWrite(DS2_16o, HIGH): digitalWrite(DS2_16o, LOW);
+    ( (note&8)  == 8 )  ? digitalWrite(DS2_8o, HIGH): digitalWrite(DS2_8o, LOW);
+    ( (note&4)  == 4 )  ? digitalWrite(DS2_4o, HIGH): digitalWrite(DS2_4o, LOW);
+    ( (note&2)  == 2 )  ? digitalWrite(DS2_2o, HIGH): digitalWrite(DS2_2o, LOW);
+    ( (note&1)  == 1 )  ? digitalWrite(DS2_1o, HIGH): digitalWrite(DS2_1o, LOW);
     digitalWrite(DS2_GATE_NEG, LOW);
   }
 
@@ -49,6 +50,21 @@ void setup() {
 
   // Serial port (MIDI), default baudrate 31250
   Serial.begin(31250);
+
+  // Initialize the note list
+  initMidiNoteList( &midiNotes, HIGHER );
+}
+
+void play( noteList_t *list ){
+  byte noteIdx = midiNoteToPlay(list);
+  playNote( list->midiNotes[noteIdx].noteNumber, list->midiNotes[noteIdx].velocity );
+  /*if (list->midiNotes[noteIdx].velocity == 0){
+    digitalWrite(DS2_1o, HIGH);
+    digitalWrite(DS2_2o, LOW);
+    }else{
+      digitalWrite(DS2_1o, LOW);
+    digitalWrite(DS2_2o, HIGH);
+    }*/
 }
 
 void loop() {
@@ -67,7 +83,8 @@ void loop() {
        //not implemented yet
     }else if ( (action==0)&&(note==0) ){ // if we received a "note off", we wait for which note (databyte)
       note=incomingByte;
-      playNote(note, 0);
+      //playNote(note, 0);
+      popNote(&midiNotes, note);
       note=0;
       velocity=0;
       action=2;
@@ -75,7 +92,8 @@ void loop() {
       note=incomingByte;
     }else if ( (action==1)&&(note!=0) ){ // ...and then the velocity
       velocity=incomingByte;
-      playNote(note, velocity);
+      //playNote(note, velocity);
+      pushNote(&midiNotes, note, velocity);
       note=0;
       velocity=0;
       action=0;
@@ -83,5 +101,7 @@ void loop() {
       //nada
     }
   }  
+
+  play( &midiNotes );
 
 }
